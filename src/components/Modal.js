b98fe791075c26b2901e7d208b0adaf478cef464
Nicolas-Upcott <nicolas-upcott@mohawkcollege.ca>
2023-01-12 18:32:12 -0500

    Simplified modal event handling

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,19 +3,15 @@ import Button from "./UserInput/Button";
 import styles from "./Modal.module.css";
 
 const Modal = (props) => {
-  const onCloseModalHandler = (event) => {
-    props.closeModal();
-  };
-
   return (
-    <div className={styles.backdrop} onClick={onCloseModalHandler}>
+    <div className={styles.backdrop} onClick={props.closeModal}>
       <div className={styles.modal}>
         <div className={styles.header}>
           <h2>Invalid input</h2>
         </div>
         <div className={styles.content}>{props.content}</div>
         <div className={styles.actions}>
-          <Button type="button" onClick={onCloseModalHandler}>
+          <Button type="button" onClick={props.closeModal}>
             Okay
           </Button>
         </div>
